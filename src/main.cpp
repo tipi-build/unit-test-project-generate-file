280e85e9996b9ed27c0cc7929c0f4e60cf0b0c99
Lambourl <65184664+Lambourl@users.noreply.github.com>
2023-10-04 21:08:58 +0200

    :wink: create a test

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+#include "../generate/generated.h"
+
+int main(int argc, char **argv) {
+    std::cout<<"the version is "<<version<<std::endl;
+   
+    return 0;
+}
